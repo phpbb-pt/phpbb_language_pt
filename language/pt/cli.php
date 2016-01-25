09825c04bd00b3f0721b49d096246178880f2815
borgesjoaquim <borgesjoaquim@gmail.com>
2016-01-25 16:16:27 +0000

    Revert "Update Final versao 2.0.4"
    This reverts commit 2e5eb3d2ecca75cfca962bb546d0ff3d6188b2cd.

--- language/pt/cli.php
+++ language/pt/cli.php
@@ -8,7 +8,7 @@
 *
 * For full copyright and license information, please see
 * the docs/CREDITS.txt file.
-* @Traduzido por: http://phpbbportugal.com - segundo as normas do Acordo Ortográfico
+*
 */
 
 if (!defined('IN_PHPBB'))
