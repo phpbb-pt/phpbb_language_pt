3ae9e6530d45adff447a8b1f46891aaec6d807d3
borgesjoaquim <borgesjoaquim@gmail.com>
2018-12-19 13:44:35 +0000

    Update phpbb 3.2.5
    Update phpbb 3.2.5

--- phpBB 3.2/language/pt/acp/permissions.php
+++ phpBB 3.2/language/pt/acp/permissions.php
@@ -54,7 +54,7 @@ $lang = array_merge($lang, array(
 
 		<br />
 
-		<p>Para mais informações sobre a configuração e a Gestão de Permissões no phpBB3, veja a secção <a href="https://www.phpbb.com/support/docs/en/3.1/ug/quickstart/permissions/">Setting permissions of our Quick Start Guide</a>.</p>
+		<p>Para mais informações sobre a configuração e a Gestão de Permissões no phpBB3, veja a secção <a href="https://www.phpbb.com/support/docs/en/3.2/ug/quickstart/permissions/">Setting permissions of our Quick Start Guide</a>.</p>
 	',
 
 	'ACL_NEVER'				=> 'Nunca',
