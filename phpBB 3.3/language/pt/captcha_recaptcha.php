286679bb8d66172383cb03f3f3ea90b2c549dff4
borgesjoaquim <borgesjoaquim@gmail.com>
2021-02-04 19:27:20 +0000

    Update captcha_recaptcha.php

--- phpBB 3.3/language/pt/captcha_recaptcha.php
+++ phpBB 3.3/language/pt/captcha_recaptcha.php
@@ -36,7 +36,7 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
-+$lang = array_merge($lang, [
+$lang = array_merge($lang, [
 	
 	// Find the language/country code on https://developers.google.com/recaptcha/docs/language
 	// If no code exists for your language you can use "en" or leave the string empty
